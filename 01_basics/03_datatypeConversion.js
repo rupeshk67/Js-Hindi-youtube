711bf3ff735e18563990348085bd38f7af2278e8
rupeshk67 <68275536+rupeshk67@users.noreply.github.com>
2023-11-14 03:45:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,29 @@
+// let score = 33;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let score = "33abv";
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*
+"33" ==> 33
+"33abc" ==> NaN (not a number)
+true ==> 1 , false ==>0
+null ==>0
+undefined ==>NaN
+*/
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+1 ==> true, 0 ==>false
+"rup" ==>true
+"" ==> false
+*/
