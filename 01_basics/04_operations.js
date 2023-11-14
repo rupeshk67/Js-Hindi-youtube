ee026d5359af0ab3a0228454d81501770fc26ad9
rupeshk67 <68275536+rupeshk67@users.noreply.github.com>
2023-11-14 09:55:52 +0000

    updates

--- 01_basics/04_operations.js
+++ 01_basics/04_operations.js
@@ -1,4 +1,36 @@
-let value = 3;
-let negValue = -value;
-console.log(negValue);
-console.log(typeof negValue);
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+// console.log(typeof negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello";
+// let str2 = " Rupesh";
+// let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
